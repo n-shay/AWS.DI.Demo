bf827b97c09ed93d082b8c5908f3b102d098c7b4
Shay Nissel <n_shay@hotmail.com>
2019-06-12 16:36:46 -0700

    Optimize service provider

--- src/AWS.DI.DependencyResolution/CurrentAssemblyServiceProvider.cs
+++ src/AWS.DI.DependencyResolution/CurrentAssemblyServiceProvider.cs
@@ -7,19 +7,20 @@ namespace AWS.DI.DependencyResolution
 {
     public class CurrentAssemblyServiceProvider : ServiceProviderBase
     {
-        protected override void LoadPackagesInternal(Container container)
-        {   
-            //var packages = this.GetType().Assembly.GetTypes()
-            //    .Where(t => typeof(IPackage).IsAssignableFrom(t) && !t.IsAbstract && !t.IsInterface)
-            //    .Select(t => (IPackage)Activator.CreateInstance(t))
-            //    .ToList();
+        protected override void RegisterInternal(Container container)
+        {
+            var packages = this.GetType().Assembly.GetTypes()
+                .Where(t => typeof(IPackage).IsAssignableFrom(t) && !t.IsAbstract && !t.IsInterface)
+                .Select(t => (IPackage)Activator.CreateInstance(t))
+                .ToList();
 
-            //foreach (var package in packages)
-            //{
-            //    package.RegisterServices(container);
-            //}
+            foreach (var package in packages)
+            {
+                package.RegisterServices(container);
+            }
 
-            container.RegisterPackages(new[] { this.GetType().Assembly });
+            // above code is faster
+            //container.RegisterPackages(new[] { this.GetType().Assembly });
         }
     }
 }
--- src/AWS.DI.DependencyResolution/ServiceProviderBase.cs
+++ src/AWS.DI.DependencyResolution/ServiceProviderBase.cs
@@ -1,40 +1,41 @@
-﻿using System.Threading.Tasks;
-using AWS.DI.Core.Concurrency;
-using SimpleInjector;
+﻿using SimpleInjector;
 
 namespace AWS.DI.DependencyResolution
 {
     public abstract class ServiceProviderBase : IServiceProvider
     {
-        private readonly ReaderWriterLock _readerWriterLock = new ReaderWriterLock();
-        private readonly Container _container;
+        private readonly object _syncRoot = new object();
+        private Container _container;
         
-        protected ServiceProviderBase()
-        {
-            _container = new Container();
-
-            // make sure execution is done in the background and holding the thread, allow the instantiation of the Lambda
-            Task.Run(() =>
-            {
-                using (_readerWriterLock.WriterLock())
-                {
-                    LoadPackagesInternal(_container);
-
-                    // TODO: don't verify in production (release)
-                    _container.Verify();
-                }
-            });
-        }
-
-        protected abstract void LoadPackagesInternal(Container container);
+        protected abstract void RegisterInternal(Container container);
 
         public T Resolve<T>()
             where T : class
         {
-            using (_readerWriterLock.ReaderLock())
+            return GetOrCreateInternal().GetInstance<T>();
+        }
+
+        private Container GetOrCreateInternal()
+        {
+            if (_container == null)
             {
-                return _container.GetInstance<T>();   
+                lock (_syncRoot)
+                {
+                    if (_container == null)
+                    {
+                        _container = new Container();
+                        
+                        RegisterInternal(_container);
+
+#if DEBUG
+                        // don't verify in release
+                        _container.Verify();
+#endif
+                    }
+                }
             }
+
+            return _container;
         }
     }
 }
